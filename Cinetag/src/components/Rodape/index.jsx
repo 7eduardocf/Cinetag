9150cff0fb45dca9883c917157023b666d96f7fd
Eduardo Frota <eduardocfrota901@gmail.com>
2025-04-07 22:54:49 -0400

    feita a criação dos componentes e importa-los na pagina de inicio

---
+++ Cinetag/src/components/Rodape/index.jsx
@@ -0,0 +1,9 @@
+import styles from "./rodape.module.css"
+function Rodape(){
+    return(
+        <footer className={styles.container}>
+            <h2 className={StyleSheet.paragrafo}>Desenvolvido por Alura</h2>
+        </footer>
+    )
+}
+export default Rodape
--- Cinetag/src/pages/Inicio/index.jsx
+++ Cinetag/src/pages/Inicio/index.jsx
@@ -1,6 +1,12 @@
+import Rodape from "../../components/Rodape"
+import Cabecalho from "../../components/Cabecalho/index.jsx"
+
 function Inicio(){
     return(
-        <h1>Olá mundo, estou em uma pagina nova e uma nova rota</h1>
+        <>
+            <Cabecalho/>
+            <Rodape/>
+        </>
     )
 }
 export default Inicio
