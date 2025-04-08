60d68f0ec2c3de3036963dc2568d737ddbaebf37
Eduardo Frota <eduardocfrota901@gmail.com>
2025-04-07 23:26:14 -0400

    feito o componente de Banner

--- Cinetag/src/pages/Inicio/index.jsx
+++ Cinetag/src/pages/Inicio/index.jsx
@@ -1,10 +1,12 @@
 import Rodape from "../../components/Rodape"
 import Cabecalho from "../../components/Cabecalho/index.jsx"
+import Banner from "../../components/Banner"
 
 function Inicio(){
     return(
         <>
             <Cabecalho/>
+            <Banner imagem="home"/>
             <Rodape/>
         </>
     )
