6a70791d8329952a3771a9f3ee21a21b0ba38733
Eduardo Frota <eduardocfrota901@gmail.com>
2025-04-08 22:59:18 -0400

    Feita a criação da lista dinamica para exibição de cards

--- Cinetag/src/pages/Inicio/index.jsx
+++ Cinetag/src/pages/Inicio/index.jsx
@@ -3,6 +3,8 @@ import Cabecalho from "components/Cabecalho/index.jsx"
 import Banner from "components/Banner"
 import Titulo from "components/Titulo"
 import Card from "components/Card"
+import videos from "json/db.json"
+import styles from "./Inicio.module.css"
 
 function Inicio(){
     return(
@@ -12,7 +14,11 @@ function Inicio(){
             <Titulo>
                 <h1>Um lugar para guardar seus vídeos e filmes!</h1>
             </Titulo>
-            <Card id="1" titulo="Gato bonifacio" capa="https://thecatapi.com/api/images/get?format-src&type=png"/>
+            <section className={styles.container}>
+                {videos.map((video) =>{
+                    return <Card {...video} key={video.id}/>
+                })}
+            </section>
             <Rodape/>
         </>
     )
